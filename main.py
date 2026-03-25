5ff42949b1492ce2acbb11266c840aa56573ea6a
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2026-03-25 14:27:26 +0800

    Finihsed flask and backend intergation. AI works

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, jsonify
+import os
+from dotenv import load_dotenv
+from openrouter import OpenRouter
+load_dotenv()
+api_key = os.getenv("API_KEY")
+ 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template('index.html')
+
+@app.route("/chatbot/msg", methods=["GET", "POST"])
+def chatbotmsg():
+    data = request.get_json()
+    if not data or "message" not in data:
+        return jsonify({
+            "error": "No Message provided"
+        }), 400
+    message = data.get('message')
+    
+    print(f"Received data from button: {message}")
+    reply = chatbot(str(message))
+    return jsonify({
+        "reply": reply
+    })
+
+def chatbot(message):
+    client = OpenRouter(
+        api_key=api_key,
+        server_url="https://ai.hackclub.com/proxy/v1",
+    )
+
+    response = client.chat.send(
+        model="qwen/qwen3-32b",
+        messages=[
+            {"role": "system", "content": "You are an extremely friendly agent named Merl, who's task is to aid with BASIC raspberry pi troubleshooting. The raspberry pi will be reffered to as its name or RPI in some cases. None the less, your job is to help with basic troubleshooting. Respond in 2 lines maximum, and keep your answers short and concise, and help the user to the best of your extent. If the user's questions extend beyond scope of your task, then respond with: I'm sorry, I can't help with that. "},
+            {"role": "user", "content": message}
+        ],
+        stream=False,
+    )
+    return(response.choices[0].message.content)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=6969)
--- static/js/script.js
+++ static/js/script.js
@@ -2,12 +2,56 @@
 const btn = document.getElementById("chatbotButton");
 const box = document.getElementById("chatbotBox");
 const input = document.getElementById("chatInput")
+const msgs = document.getElementById("chatMessages");
+
 btn.addEventListener("click", () => {
       box.classList.toggle("hidden");
 });
 const send = document.getElementById("chatboxSend");
 send.addEventListener("click", () => {
   console.log(input.value);
-  input.value = "";
-});
+  const message = input.value;
+  const userMsg = document.createElement("p");
+  userMsg.textContent = message;
+  userMsg.classList.add("chat-msg-user");
+  msgs.appendChild(userMsg);
 
+  const typing = showTyping();
+  fetch("/chatbot/msg", {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      message: message
+    })
+  })
+  .then(res => {
+    console.log('Status:', res.status);
+    return res.json();
+  })
+  .then(data => {
+    removeTyping()
+    console.log("Response from server:", data);
+    const msg = document.createElement("p");
+    msg.textContent = data.reply;
+    msg.classList.add("chat-msg")
+    msgs.appendChild(msg);
+    msgs.scrollTop = msgs.scrollHeight;
+  });
+  input.value = "";
+  
+});
+function showTyping() {
+  const typing = document.createElement("p");
+  typing.id = "typing-indicator";
+  typing.classList.add("chat-msg", "typing-dots");
+  msgs.appendChild(typing);
+  return typing;
+}
+function removeTyping() {
+  const typing = document.getElementById("typing-indicator");
+  if (typing) {
+    typing.remove();
+  }
+}
